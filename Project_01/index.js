0f3faf6c8456c3643fc20ddc058e9407de4bc3f3
abdullahkhalil00 <abdullahkhalil4615@gmail.com>
2026-07-14 08:35:31 +0500

    Before Rest API

---
+++ Project_01/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+app.get('/',(req,res) =>{
+    res.send("Welcome")
+})
+
+const Port = 8000;
+app.listen(Port , () => console.log("Server Started"))
